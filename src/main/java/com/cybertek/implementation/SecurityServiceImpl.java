93bb0c30f926e33dbb7d96b0884bb6f97643ce38
ozzycybertek <ozzy@cybertekschool.com>
2021-01-20 19:50:45 -0500

    Ticketing Project Security

--- src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -1,5 +1,8 @@
 package com.cybertek.implementation;
 
+import com.cybertek.entity.User;
+import com.cybertek.entity.common.UserPrincipal;
+import com.cybertek.repository.UserRepository;
 import com.cybertek.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +11,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityServiceImpl implements SecurityService {
 
+    private UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+
+        User user = userRepository.findByUserName(s);
+
+        if(user==null){
+            throw new UsernameNotFoundException("This user does not exists");
+        }
+
+        return new UserPrincipal(user);
     }
 }
