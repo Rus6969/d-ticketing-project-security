a900beee5d06aab38f04d63f7933d7e7c5b52112
ozzycybertek <ozzy@cybertekschool.com>
2021-01-20 19:33:59 -0500

    Ticketing Project Security

---
+++ src/main/java/com/cybertek/implementation/SecurityServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cybertek.implementation;
+
+import com.cybertek.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
